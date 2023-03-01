9aae1666f126a90fa42ed483409b7f1a33913e1d
wolfljj <wolfljj007@qq.com>
2023-03-01 11:08:08 +0800

    add hello event

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-io.on('connection', () => { /* … */ });
+io.on('connection', (socket) => {
+  socket.emit("hello", "world");
+});
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
